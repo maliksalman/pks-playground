9d816fa1dafe96e4ef79e8d404f8d56f2cc40e9b
Salman Malik <smalik@pivotal.io>
2018-05-17 17:25:22 -0400

    Updating to latest docker messsssage-grape images

--- kubernetes-kafka/kafka-server-start.sh
+++ kubernetes-kafka/kafka-server-start.sh
@@ -10,6 +10,13 @@ else
     exit 1
 fi
 
+# start the kafka server
+kafka/bin/kafka-server-start.sh /usr/etc/kafka/server.properties \
+	--override broker.id=$ORD \
+	--override advertised.listeners=PLAINTEXT://$(hostname -f):9092 \
+	--override listeners=PLAINTEXT://0.0.0.0:9092 \
+	--override zookeeper.connect=zk:$ZK_SERVICE_PORT
+
 ## Start the broker with multiple listeners 0 for external consumption
 #NODE_PORT=$(cat /usr/etc/kafka/kafka-${ORD}.nodeport)
 #kafka/bin/kafka-server-start.sh /usr/etc/kafka/server.properties \
@@ -19,10 +26,3 @@ fi
 #	--override advertised.listeners=EXT://${NODE_HOSTNAME}:${NODE_PORT},INT://${HOST}.${DOMAIN}:9092 \
 #	--override listeners=EXT://0.0.0.0:${NODE_PORT},INT://0.0.0.0:9092 \
 #	--override zookeeper.connect=zk-cs:$ZK_CS_SERVICE_PORT
-
-# start the kafka server
-kafka/bin/kafka-server-start.sh /usr/etc/kafka/server.properties \
-	--override broker.id=$ORD \
-	--override advertised.listeners=PLAINTEXT://$(hostname -f):9092 \
-	--override listeners=PLAINTEXT://0.0.0.0:9092 \
-	--override zookeeper.connect=zk-cs:$ZK_CS_SERVICE_PORT
